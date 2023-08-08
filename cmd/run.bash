6e98eba5102afcee6b6291556d3422dac61205ee
YakultGo <god1239426256@163.com>
2023-08-08 10:22:21 +0800

    基础接口的service和web端已完成

--- cmd/run.bash
+++ cmd/run.bash
@@ -1,6 +1,6 @@
 # service端微服务
-nohup go run ./cmd/service/user/main.go &
-nohup go run ./cmd/service/feed/main.go &
+nohup go run ./cmd/service/user/main.go > /dev/null 2>&1 &
+nohup go run ./cmd/service/feed/main.go > /dev/null 2>&1 &
 # web端微服务
-nohup go run ./cmd/web/user/main.go &
-nohup go run ./cmd/web/feed/main.go &
+nohup go run ./cmd/web/user/main.go > /dev/null 2>&1 &
+nohup go run ./cmd/web/feed/main.go > /dev/null 2>&1 &
